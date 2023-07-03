efe613199ac570d83c8f230c711366016fd68a29
DSMalamov <dsmalamov@gmail.com>
2023-07-03 22:21:41 +0300

    ProblemsSolved

---
+++ JSSyntaxFundamentals-Exercises/08.ReversedChars.js
@@ -0,0 +1,5 @@
+function charToString(a, b, c) {
+  console.log(`${c} ${b} ${a}`);
+}
+
+charToString("a", "b", "c");
---
+++ JSSyntaxFundamentals-Exercises/16.SortingNumbers.js
@@ -0,0 +1,21 @@
+function sortingNum(arr) {
+    
+    const sortedArr = arr.sort((a, b) => a - b);
+    const sAlength = arr.length;
+    const resultArr = [];
+
+    for (let index = 0; index < sAlength; index++) {
+        
+        if (index % 2 === 0) {
+            resultArr.push(sortedArr.shift());
+        }
+        else {
+            resultArr.push(sortedArr.pop());
+        }  
+    }
+
+    console.log(sortedArr);
+}
+
+sortingNum([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
+
