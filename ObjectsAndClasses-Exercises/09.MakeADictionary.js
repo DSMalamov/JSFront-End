4e262e4a3855b74b41d0adb0706c725566f92e59
DSMalamov <dsmalamov@gmail.com>
2023-07-13 22:42:37 +0300

    ProblemsSolved

---
+++ ObjectsAndClasses-Exercises/09.MakeADictionary.js
@@ -0,0 +1,28 @@
+function dictionary(input) {
+  const dict = input.reduce((acc, curr) => {
+    const term = JSON.parse(curr);
+    acc = { ...acc, ...term };
+    return acc;
+  }, {});
+
+  // ALTERNATIVE SOLUTION
+  //   let dictionary = {};
+
+  //   input.forEach((jsonString) => {
+  //     const term = JSON.parse(jsonString);
+  //     dictionary = Object.assign(dictionary, term);
+  //   });
+
+  const sotredDictTerm = Object.keys(dict).sort();
+  sotredDictTerm.forEach((term) =>
+    console.log(`Term: ${term} => Definition: ${dict[term]}`)
+  );
+}
+
+dictionary([
+  '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+  '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."}',
+  '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+  '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+  '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} ',
+]);
